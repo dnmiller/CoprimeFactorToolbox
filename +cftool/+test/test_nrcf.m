a8470d2349513325a9aa5f456c4e18840d7ea0fa
Dan Miller <dnmiller@gmail.com>
2015-03-15 20:29:06 -0700

    Cleanup nrcf test

--- +cftool/+test/test_nrcf.m
+++ +cftool/+test/test_nrcf.m
@@ -14,16 +14,26 @@ function testInputValidation(testCase)
 end
 
 function testDiscreteTimeAlgorithm(testCase)
+% Test the discrete-time normalized right coprime factorization.
+    runTest(testCase, @drss);
+end
+
+function testContinuousTimeAlgorithm(testCase)
+% Test the continuous time normalized right coprime factorization.
+    runTest(testCase, @rss);
+end
+
+function runTest(testCase, sysGen)
     dropIn = false;
-    function run_test(n, nu, ny)
+    function run(n, nu, ny)
         import cftool.*;
 
         % Run over this many random systems.
-        Nruns = 50;
+        Nruns = 8;
 
         % Continuous time
         for i = 1:Nruns
-            G0 = drss(n, ny, nu);
+            G0 = sysGen(n, ny, nu);
             try
                 [N, M] = nrcf(G0);
             catch err
@@ -48,50 +58,7 @@ function testDiscreteTimeAlgorithm(testCase)
     for n = [1, 2, 6]
         for nu = [1, 2, 6]
             for ny = [1, 2, 6]
-                run_test(n, nu, ny);
-            end
-        end
-    end
-end
-
-function testContinuousTimeAlgorithm(testCase)
-% Test the continuous time normalized right coprime factorization.
-return
-    dropIn = false;
-    function run_test(n, nu, ny)
-        import cftool.*;
-
-        % Run over this many random systems.
-        Nruns = 8;
-
-        % Continuous time
-        for i = 1:Nruns
-            G0 = rss(n, ny, nu);
-            try
-                [N, M] = nrcf(G0);
-            catch err
-                if dropIn
-                    keyboard;
-                    [N, M] = nrcf(G0);
-                else
-                    rethrow(err)
-                end
-            end
-            
-            if ~isIoEqual(N/M, G0)
-                keyboard;
-                testCase.assertFail;
-            end
-            
-            R = minreal(balreal(M'*M + N'*N), [], false);
-            testCase.assertLessThan(abs(dcgain(R) - eye(nu)), sqrt(eps));
-        end
-    end
-
-    for n = [1, 2, 6]
-        for nu = [1, 2, 6]
-            for ny = [1, 2, 6]
-                run_test(n, nu, ny);
+                run(n, nu, ny);
             end
         end
     end
